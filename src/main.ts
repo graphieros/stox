a2f69e1beedb61b8dd8f32cde411553d03076f06
graphieros <alec.lloyd.probert@gmail.com>
2022-01-26 08:08:59 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
+import WaveUI from "wave-ui";
+import "wave-ui/dist/wave-ui.css";
+
+const app = createApp(App).use(router);
+
+new WaveUI(app, {
+  // Some Wave UI options.
+});
+
+app.mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const key = import.meta.env.VITE_API_KEY;
+
+const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${key}`;
+
+fetch(url)
+  .then((response) => {
+    if (response.status !== 200) {
+      console.error("Something weent wrong", response.status);
+      return;
+    }
+
+    response.json().then((data) => {
+      console.log(data);
+    });
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+</script>
+
+<template>HOME</template>
